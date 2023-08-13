a479e5b98b86ff97660e639e624a405af037c22b
OG <og@hiimog.com>
2023-08-12 20:00:18 -0400

    Add gsap and mui styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,28 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import { makeStyles } from "@mui/styles";
+
+
+const useStyles = makeStyles({
+  root: {
+    height: "100vh",
+  },
+  upperHalf: {
+    height: "50%",
+    backgroundColor: "red"
+  },
+  lowerHalf: {
+    height: "50%",
+    backgroundColor: "blue"
+  }
+})
 
 export default function Index() {
-  return <Container maxWidth="lg"></Container>;
+  const classes = useStyles();
+
+  return <Container maxWidth="lg">
+    <Container classes={[classes.upperHalf]}></Container>
+    <Container classes={[classes.lowerHalf]}></Container>
+  </Container>;
 }
