06ab6b3e410a3fe44010c8f232ecd2b29bd2dee2
OG <og@hiimog.com>
2023-08-24 00:00:42 -0400

    Add first navigation attempt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-import Image from 'next/image'
-
+import styles from './home.module.scss';
+import Link from 'next/link'
 export default function Home() {
   return (
-    <div className='fill-red-700 min-h-full min-w-full'></div>
-  )
+      <nav className={styles.nav}>
+        <ul>
+          <li>hi, i'm og</li>
+          <li><Link href="/about">about</Link></li>
+          <li><Link href="/blog">blog</Link></li>
+          <li><Link href="/contact">contact</Link></li>
+        </ul>
+      </nav>
+    )
 }
