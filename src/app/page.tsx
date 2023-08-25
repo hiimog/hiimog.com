196e77801d7fe4f097ed9e5963dd3d37316876da
OG <og@hiimog.com>
2023-08-24 23:58:45 -0400

    Adjust staircase effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,11 @@ import styles from './home.module.scss';
 import Link from 'next/link'
 export default function Home() {
   return (
-      <nav className={styles.nav}>
-        <ul>
-          <li>hi, i'm og</li>
-          <li><Link href="/about">about</Link></li>
-          <li><Link href="/blog">blog</Link></li>
-          <li><Link href="/contact">contact</Link></li>
-        </ul>
+      <nav className={styles.navMain}>
+        <h1 className={styles.titleLine}>hi, i'm og</h1>
+        <h2 className={styles.aboutLink}><Link href="/about">about</Link></h2>
+        <h2 className={styles.blogLink}><Link href="/blog">blog</Link></h2>
+        <h2 className={styles.contactLink}><Link href="/contact">contact</Link></h2>
       </nav>
     )
 }
