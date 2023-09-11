f0c10004ae28ce188bf23440dbb90fdfd4fc2071
OG <og@hiimog.com>
2023-09-10 23:59:50 -0400

    Add additional information to about page

--- hiimog/src/app/about/page.tsx
+++ hiimog/src/app/about/page.tsx
@@ -1,18 +1,45 @@
 export default function () {
   return (
     <>
-      <h1>about og</h1>
+      <h1>About OG</h1>
+      <em>
+        Everything on this site is my own opinion and in no way reflects the values/views of my
+        past/present/future employers.
+      </em>
+      <h2>General Information</h2>
       <ul>
+        <li>life long tinkerer/diy'er</li>
         <li>google software engineer</li>
         <li>penn state alum</li>
-        <li>life long tinkerer</li>
         <li>wood worker</li>
         <li>(mediocre) chess player</li>
+        <li>keeb dork</li>
+        <li>corgis</li>
+        <li>art/tattoos/graffiti</li>
       </ul>
-      <em>
-        everything on this site is my own opinion and in no way reflects the
-        values/views of my employer
-      </em>
+      <h2>Why OG?</h2>
+      <p>
+        When I was in college, there was another person in the friend group with the same name. I was
+        older and someone called me OG and it stuck.
+      </p>
+      <h2>Can I Hire You?</h2>
+      <p>
+        I mostly get absurd requests which I no longer respond to e.g. "Make me a
+        facebook/twitter/whatever clone for about $3.50." If you have serious inquiries please email:
+        <a href="mailto:og+inquiries@hiimog.com">og+inquiries@hiimog.com</a>.
+      </p>
+      <h2>Technology Areas of Interest</h2>
+      <ul>
+        <li>artificial intelligence</li>
+        <li>competitive programming</li>
+        <li>information security</li>
+        <li>language design</li>
+        <li>observability</li>
+        <li>operating systems</li>
+        <li>reverse engineering</li>
+        <li>software design</li>
+        <li>systems design</li>
+      </ul>
     </>
   );
 }
