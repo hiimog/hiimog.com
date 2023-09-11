beffb61453411daca804255369de3e2388d1a001
OG <og@hiimog.com>
2023-09-10 20:45:53 -0400

    Add links to about and posts These are not implemented yet

--- hiimog/src/app/page.tsx
+++ hiimog/src/app/page.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link"
+
 export default function () {
-  return <h1>hi, im og</h1>
+  const links = [
+    {display: "about", href: "/about"},
+    {display: "posts", href: "/posts"},
+  ].map(l => <li><Link href={l.href}>{l.display}</Link></li>)
+
+  return (
+    <>
+    <h1>hi, im og</h1>
+    <nav>
+      <ul>
+        {links}
+      </ul>
+    </nav>
+    </>
+  )
 }
