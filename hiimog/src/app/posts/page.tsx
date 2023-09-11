2cf339b0f6fa4d3a143cd0ee1501084ee76f265d
OG <og@hiimog.com>
2023-09-10 21:16:04 -0400

    Add logging fundamentals dummy blog post

--- hiimog/src/app/posts/page.tsx
+++ hiimog/src/app/posts/page.tsx
@@ -2,7 +2,8 @@ import Link from "next/link"
 
 export default function () { 
     const posts = [
-        {title: "making a blog", slug: "making-a-blog"}
+        {title: "making a blog", slug: "making-a-blog"},
+        {title: "logging fundamentals", slug: "logging-fundamentals"},
     ].map(p => <li><Link href={`/posts/${p.slug}`}>{p.title}</Link></li>)
     return (
         <>
