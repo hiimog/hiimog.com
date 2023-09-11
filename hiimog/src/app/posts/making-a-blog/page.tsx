336803cee2f060b1c334caa160e6762626a342b2
OG <og@hiimog.com>
2023-09-10 22:04:07 -0400

    Add example of using images

--- hiimog/src/app/posts/making-a-blog/page.tsx
+++ hiimog/src/app/posts/making-a-blog/page.tsx
@@ -1,3 +1,8 @@
+import errorex from "./images/errorex.png"
+
 export default function () {
-  return <h1>Making A Blog</h1>;
+  return <>
+  <h1>Making A Blog</h1>
+  <img src={errorex.src} />
+  </>;
 }
