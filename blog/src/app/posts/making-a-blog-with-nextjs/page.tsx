3dc82e6499e354e5d31b2485cfb3d85aad8252c9
OG <og@hiimog.com>
2023-11-12 14:53:26 -0500

    Add table of contents to making a blog post

--- blog/src/app/posts/making-a-blog-with-nextjs/page.tsx
+++ blog/src/app/posts/making-a-blog-with-nextjs/page.tsx
@@ -1,3 +1,23 @@
 export default function MakingABlogWithNextJs() {
-    return <h1>Making A Blog With NextJS</h1>
+    return (<>
+    <h1>Making A Blog With NextJS</h1>
+    <p id="tldr">
+        In this post we will create a simple statically generated blog using 
+        <a href="https://nextjs.org">NextJS</a> and hosting the site on <a href="https://pages.github.com/">GitHub Pages</a>.
+        We will be building this from the ground up and not relying on any out of the box solution like <a href="https://jekyllrb.com/docs/github-pages/">Jekyll</a>
+    </p>
+        {/* TODO: Make this <nav> and make these links and call it TOC */}
+        <h2>Outline</h2>
+        <ul>
+            <li>Create GitHub Account</li>
+            <li>Install Software</li>
+            <li>Create Project Structure</li>
+            <li>Add Content</li>
+            <li>Add Styles</li>
+        </ul>
+
+        <section id="create-github-account">
+
+        </section>
+    </>)
 }
